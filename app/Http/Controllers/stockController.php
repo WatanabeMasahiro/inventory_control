ec71a12e6d93c700d65d17021fa166b420e88b24
WatanabeMasahiro <2c.nave.writer@gmail.com>
2020-12-24 02:38:28 +0900

    ビューテンプレート【ホーム画面】の初期表示まで

---
+++ app/Http/Controllers/stockController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class stockController extends Controller
+{
+
+    public function homeGet(Request $request)
+    {
+        return view('home', );
+    }
+
+    public function homePost(Request $request)
+    {
+        return view('home', compact());
+    }
+
+    public function registerGet(Request $request)
+    {
+        return view('register', compact());
+    }
+
+    public function registerPost(Request $request)
+    {
+        return view('register', compact());
+    }
+
+    public function deleteGet(Request $request)
+    {
+        return view('delete', compact());
+    }
+
+    public function deletePost(Request $request)
+    {
+        return view('delete', compact());
+    }
+
+
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('inventory_control', 'stockController@homeGet');
+Route::post('inventory_control', 'stockController@homePost');
+
+Route::get('inventory_control/register', 'stockController@registerGet');
+Route::post('inventory_control/register', 'stockController@registerPost');
+
+Route::get('inventory_control/delete', 'stockController@deleteGet');
+Route::post('inventory_control/delete', 'stockController@deletePost');
+
+// Route::get('Inventory_control', 'stockController@');
+// Route::post('Inventory_control', 'stockController@');
+
+// Auth::routes();

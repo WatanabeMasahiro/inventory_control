8a3287465ffe678028902a921da573a3dd5aaa0b
WatanabeMasahiro <2c.nave.writer@gmail.com>
2020-12-24 05:09:57 +0900

    views/home.blade.phpのHTML文追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,49 @@
 <head>
 </head>
 <body>
-    <h1>Hello</h1>
+    <div class="container header">
+    <div class="row">
+        <h1>在庫管理くん</h1>
+        
+        <div>
+            <p><a class="" href="">ログアウト</a> / <a class="" href="">退会</a></p>
+        </div>
+    </div>
+    </div>  <!-- header -->
+
+    <hr>
+
+    <div class="container mainContents">
+    <div class="row">
+
+        <buttom class="">データ登録</buttom>
+
+        <table>
+            <tr>
+                <th>カテゴリー</th><th>品　物</th><th>個　数</th><th>備　考</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+
+        <p>ここにページネーション</p>
+
+    </div>
+    </div>  <!-- mainContents -->
+
+    <hr>
+
+    <div class="container footer">
+    <div class="row">
+
+        <p>Copyright - M-Watanabe, 2020.</p>
+
+    </div>
+    </div> <!-- footer -->
+
 </body>
 </html>
